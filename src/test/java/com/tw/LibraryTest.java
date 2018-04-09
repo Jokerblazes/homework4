a3e9e7b32d5e4edc2faf232e7aa827fc009ac584
Jokerblazes <zcd123@qq.com>
2018-04-09 11:23:00 +0800

    测试数据部分错误修改。

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.io.*;
 import java.util.LinkedList;
-import java.util.Scanner;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -79,8 +78,13 @@ public class LibraryTest {
                 "14: 25\n" +
                 "14, 25, 23, 50\n" +
                 "3";
+        try {
+            System.setIn(new ByteArrayInputStream(in.getBytes("utf-8")));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         Library library = new Library();
-//        library.showPage();
+        library.showPage();
         String result = "1. 添加学生\n" +
                 "2. 生成成绩单\n" +
                 "3. 退出\n" +
@@ -99,17 +103,21 @@ public class LibraryTest {
                 "请输入你的选择（1～3）：\n" +
                 "请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n" +
                 "学生小白的成绩被添加\n" +
+                "1. 添加学生\n" +
+                "2. 生成成绩单\n" +
+                "3. 退出\n" +
+                "请输入你的选择（1～3）：\n"+
                 "请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n" +
                 "请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n" +
                 "成绩单\n" +
                 "姓名|科学|数学|平均分|总分\n" +
                 "========================\n" +
-                "小王|14|89|51.5|103\n" +
-                "小李|80|70|75|150\n" +
-                "小白|90|70|80|160\n" +
+                "小王|14|89|51.50|103\n" +
+                "小李|80|70|75.00|150\n" +
+                "小白|90|70|80.00|160\n" +
                 "========================\n" +
-                "全班总分平均数：68.83\n" +
-                "全班总分中位数：75\n" +
+                "全班总分平均数：137.67\n" +
+                "全班总分中位数：150.00\n" +
                 "1. 添加学生\n" +
                 "2. 生成成绩单\n" +
                 "3. 退出\n" +
